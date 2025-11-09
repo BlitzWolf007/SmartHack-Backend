6a796c276f06853456415db774a8573b4a349e1d
obamas-lastname <131595909+obamas-lastname@users.noreply.github.com>
2025-11-09 08:48:39 +0200

    add setup

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+python3 -m venv .venv
+source .venv/bin/activate
+pip install --upgrade pip
+pip install -r server/requirements.txt > /dev/null
+
+python3 -m server.seed
+uvicorn server.app:app --host 0.0.0.0 --port 8000 --reload &
+
+
+cd frontend
+npm i --silent
+npm run build
+npm run dev
